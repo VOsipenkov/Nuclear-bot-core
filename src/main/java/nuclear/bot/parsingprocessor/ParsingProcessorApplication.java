9c2e702e4a99436361bc858e2a32fd3431bad651
VOsipenkov <jumpViaOcean@gmail.com>
2022-11-21 14:35:50 +0300

    init

---
+++ src/main/java/nuclear/bot/parsingprocessor/ParsingProcessorApplication.java
@@ -0,0 +1,13 @@
+package nuclear.bot.parsingprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParsingProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParsingProcessorApplication.class, args);
+	}
+
+}
