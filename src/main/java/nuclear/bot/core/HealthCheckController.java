cd67c5955d6d3e4b9fe8c0572222bf66c5de1bc4
VOsipenkov <jumpViaOcean@gmail.com>
2023-07-28 15:07:34 +0300

    Nuclearbot-11: add configmaps for services

--- src/main/java/nuclear/bot/core/HealthCheckController.java
+++ src/main/java/nuclear/bot/core/HealthCheckController.java
@@ -1,12 +1,22 @@
 package nuclear.bot.core;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthCheckController {
+
+    @Value("${application.name}")
+    private String name;
+
     @GetMapping(value = "core/health")
-    public String healthCheck(){
-        return "Core health ok";
+    public String healthCheck() {
+        return "Core health ok\n";
+    }
+
+    @GetMapping(value = "/core/config")
+    public String configCheck() {
+        return name + "\n";
     }
 }
