8cf4f2082585d7077f7277d95a7822e1dd680b88
VOsipenkov <JumpViaOcean@gmail.com>
2024-02-23 03:20:08 +0300

    Nuclear-bot-telegram-4:  improve status report (#8)

---
+++ src/main/resources/db/migration/V4__update_normal_value.sql
@@ -0,0 +1,5 @@
+update normal_value set parseragentname='https://www.saveecobot.com/radiation-maps'
+                    where parseragentname='https://www.saveecobot.com/';
+
+update normal_value set parseragentname='https://www.radon.ru/online-map/?city=6'
+                    where parseragentname='don.ru/online-map/';
