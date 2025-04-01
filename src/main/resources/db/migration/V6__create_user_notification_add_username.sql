240520b73d146adc23838e4b11530d3b00901b62
VOsipenkov <JumpViaOcean@gmail.com>
2025-04-01 18:37:43 +0300

    Nuclear-bot-23:  Enabled every morning notification (#11)

---
+++ src/main/resources/db/migration/V6__create_user_notification_add_username.sql
@@ -0,0 +1,3 @@
+alter table USER_NOTIFICATION_INFO add username varchar null;
+alter table USER_NOTIFICATION_INFO add first_name varchar null;
+alter table USER_NOTIFICATION_INFO add last_name varchar null;
---
+++ src/main/resources/db/migration/V8__alter_normal_value.sql
@@ -0,0 +1,2 @@
+insert into normal_value(id, parseragentname, normalvalue, parserAgentUrl)
+values (gen_random_uuid(), 'www.radon.ru', 40, 'https://www.radon.ru/online-map/?city=6');
